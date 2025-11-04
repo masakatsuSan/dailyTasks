97bf989d506a71894d86fbed33b8d526b594b5ad
masakatsuSan <heyysumith@gmail.com>
2025-11-04 12:08:51 +0530

    updates

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,3 +1,16 @@
 // prompt("Hello world ?")
-var a = prompt("Enter your name")
-console.log(a)
+var a = prompt("Enter your name");
+console.log(typeof a);
+
+// object kya hota hain .... ek bande ke bare mein kai sare battein ko store kanre wale ko hum object khete hain 
+// {
+//     name : "Sumith",
+//     age  : 20,
+//     class : "2nd year",
+// }
+
+// NaN is output if you have mix number with the string 
+// prompt mein app jo bhi daloge voh ek string ban jayega no matter vo ek number hi q na ho 
+// ek array/null ka type object hota hain 
+// and ek object ka type bhi object hi hota hain 
+// primitive and reference value ---> agar {bracket agaya then vo ek reference value hain warna vo ek primitive vaue hain like list , objects ....}
