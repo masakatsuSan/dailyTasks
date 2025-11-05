e663e4821f9897bfeaf00e0c7c18e64f25fb6de3
masakatsuSan <heyysumith@gmail.com>
2025-11-05 13:13:14 +0530

    day29questionpracitice

---
+++ JAVASCRIPT/question_practice/29_oct.js
@@ -0,0 +1,45 @@
+// console.log(a)
+// let a = 10
+
+// test()
+// function test(){
+//     console.log("Hello world")
+// }    // this will work as the fuction can be operated even when its is call before the fuction called 
+
+// hello()
+// var hello = function hello(){
+//     console.log("Heloo")
+// }
+
+// console.log(a)
+// var a = 10 
+//What gets hoisted? --> var a the varible and const get broke into two parts console.log(var) var a = 10 --> var a and a = 10 
+//What does not get hoisted fully? --> a = 10
+
+// var age = prompt("Enter your age user ?")
+// if (age>18){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Minor")
+    
+// }
+
+// var marks = prompt("Enter your number ?")
+// if (marks>=90){
+//     console.log("A grade");
+// }
+// else if (marks >= 75){
+//     console.log("B grade");   
+// }
+// else if (marks < 50){
+//     console.log("C grade") 
+// }
+
+// let score = 35
+// let results = score<30 ? "fail" : "Pass"
+// console.log(results);
+
+let temp = 28
+let results = temp > 30 ? "Hot" : "Pleasent"
+console.log(results)
