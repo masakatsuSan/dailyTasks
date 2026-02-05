efefafad01fbc5a1c6d285ba8c0a1bbba14ca801
masakatsuSan <heyysumith@gmail.com>
2026-02-05 10:35:57 +0530

    day53done

--- FRONTEND/JAVASCRIPT/DAY52/ciberpunnk/script.js
+++ FRONTEND/JAVASCRIPT/DAY52/ciberpunnk/script.js
@@ -1,12 +1,22 @@
-const para = document.querySelector("p")
-const text = para.innerHTML
-const alpha = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()"
-console.log(alpha.length)
-para.addEventListener("mouseenter",()=>{
-    setInterval(() => {
-        const str = text.split("").map((char,index)=>{
-            return alpha.split("")[Math.floor(Math.random()*47)]
-        }).join("")
-        para.innerHTML = str
-    }, 300);
-})
+const p = document.querySelector("p")
+const text = p.innerText 
+const characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+setInterval(()=>{})
+//math.floor(23.09)-->23
+let iteration = 0
+function randomText(){
+    const str = text.split("").map((char,index)=>{
+        if(index<iteration){
+            return char //logic : the index is kept on increasing so when iteration reach the text index then the char is return not the number .. 
+        }
+        return characters.split("")[Math.floor(Math.random()*characters.length)]
+    }).join("")
+    // console.log(str)
+    p.innerText = str
+    iteration += 0.5
+    // console.log(iteration)
+}
+
+setInterval(() => {
+    randomText()
+}, 100);
