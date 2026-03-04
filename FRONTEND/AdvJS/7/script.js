de006d6080a45c95a0f540c30c82b6684458952e
masakatsuSan <heyysumith@gmail.com>
2026-03-04 16:27:11 +0530

    63done

---
+++ FRONTEND/AdvJS/7/script.js
@@ -0,0 +1,17 @@
+function abcd(){
+  let a = 13
+  console.log(a)
+}
+abcd()
+
+// run time error : a.age gives nothing
+// syntext error let ko lef likh diya hain  
+// logical error : simple logic incorrect hain yaha 
+
+try{
+  conosle.log("hello")
+}
+catch(err){
+  console.log(err)
+}
+ 
