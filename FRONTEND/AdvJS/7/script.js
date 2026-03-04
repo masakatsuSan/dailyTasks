3e20b453d782b176fc09c5d849f591b9c71cc28c
masakatsuSan <heyysumith@gmail.com>
2026-03-04 16:36:18 +0530

    63done

--- FRONTEND/AdvJS/7/script.js
+++ FRONTEND/AdvJS/7/script.js
@@ -7,11 +7,19 @@ abcd()
 // run time error : a.age gives nothing
 // syntext error let ko lef likh diya hain  
 // logical error : simple logic incorrect hain yaha 
+ 
+try{
+  conole.log(
+    "hello"
+  )
+}catch(err){
+  console.log("this is from catch msg ")
+}finally{ // will run evern if either works 
+  console.log("yehh le chal gaya")
+} 
 
 try{
-  conosle.log("hello")
+  consol.log("phela")
+}catch(err){
+  console.error(new Error("some thing went from our side that not your fault"))
 }
-catch(err){
-  console.log(err)
-}
- 
